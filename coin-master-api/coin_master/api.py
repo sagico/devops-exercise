870985f23ebf94b826a421e07783f71fe0c4b39b
Yehuda Chikvashvili <yehudaac1@gmail.com>
2022-03-04 00:23:25 +0200

    Add debugging capability

--- coin-master-api/coin_master/api.py
+++ coin-master-api/coin_master/api.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
+from loguru import logger
 
 from .config import CoinMasterSettings
 from .router import router
 
 
-def validate_configuration():
-    CoinMasterSettings()
-
-
 def create_app() -> FastAPI:
-    validate_configuration()
+    settings = CoinMasterSettings()
+    logger.info(f"Starting Coin Master API URL={settings.exchange_base_url} API_KEY={settings.exchange_api_key}")
     app = FastAPI()
     app.include_router(router=router)
     
